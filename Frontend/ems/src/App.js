2274756c164405a53b2232a144123c34e9de6c25
Joseph-Souaiby <jgsouaiby@gmail.com>
2024-03-28 00:36:33 +0200

    Basic Template

---
+++ Frontend/ems/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <hr />
+      <h1>Employee Management System</h1>
+      <hr />   
+      <div className="employee-table">
+      </div>
+
+
+    </div>
+  );
+}
+
+export default App;
