57292c5b3792c0193ba87d6a4a9f8821824366da
Joseph-Souaiby <jgsouaiby@gmail.com>
2024-04-20 17:50:02 +0300

    Task Report

--- Frontend/ems/src/EmployeeTaskReport.js
+++ Frontend/ems/src/EmployeeTaskReport.js
@@ -2,20 +2,32 @@ import { useEffect, useState, useCallback} from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Table from 'react-bootstrap/Table';
+import { Chart } from "react-google-charts";
+import ProgressBar from 'react-bootstrap/ProgressBar';
 
 const SERVER_URL = 'http://127.0.0.1:5000'
+
+
 
 
 function EmployeeTaskReport({empid}) {
 
   const [show, setShow] = useState(false);
   const [tasks, setTasks] = useState([]);
+  const [tasks2, setTasks2] = useState([]);
 
   const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const handleShow = () => {
+    if (tasks.length > 0) {
+      setShow(true);
+    }
+    else{
+      alert("No tasks to show");
+    }
+  }
 
   const fetchTasks = useCallback(async () => {
-    const response = await fetch(`${SERVER_URL}/getTasksForEmployee?emp_id=${empid}`);
+    const response = await fetch(`${SERVER_URL}/getTaskUpdatesForEmployee?emp_id=${empid}`);
     if (response.ok) {
         const data = await response.json();
         console.log(data);
@@ -23,9 +35,51 @@ function EmployeeTaskReport({empid}) {
     } else {
         console.error('Failed to fetch tasks:', response.status);
     }
+
+    const response2 = await fetch(`${SERVER_URL}/getTasksForEmployee?emp_id=${empid}`,
+        { method: 'GET' }
+      );
+    if (response2.ok) {
+        const data = await response2.json();
+        console.log(data);
+        setTasks2(data);
+    } else {
+        console.error('Failed to fetch tasks:', response2.status);
+    }
 }, [empid]);
 
   useEffect(() => { fetchTasks(); },[show, fetchTasks]);
+
+  const color = (completion,weight) => {
+    if (weight == completion){
+      return "success"
+    }
+    else{
+      return "info"
+    }
+  }
+
+  // const data = [
+  //   ["Element", "Density", { role: "style" }],
+  //   ["Copper", 8.94, "#b87333"], // RGB value
+  //   ["Silver", 10.49, "silver"], // English color name
+  //   ["Gold", 19.3, "gold"],
+  //   ["Platinum", 21.45, "color: #e5e4e2"], // CSS-style declaration
+  // ];
+
+  const data = [
+    ['Task', 'Progress Percentage', { role: 'style' }],
+    ...tasks.map(task => [task.name + "\nDate: " + task.datetime, parseInt(task.percentage, 10), 'color: blue'])
+  ];
+
+  // Chart options
+  const options = {
+    title: "Task Progress",
+    bar: { groupWidth: "95%" },
+    legend: { position: "none" },
+  };
+
+
 
 
   return (
@@ -36,30 +90,40 @@ function EmployeeTaskReport({empid}) {
         Task Report
       </Button>
 
-      <Modal show={show} onHide={handleClose}>
+      <Modal show={show} onHide={handleClose} size="lg">
         <Modal.Header closeButton>
           <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <Table striped bordered hover>
+        
+
+        <Chart
+      chartType="ColumnChart"
+      width="100%"
+      height="400px"
+      data={data}
+      options={options}
+    />
+    <Table striped bordered hover>
       <thead>
         <tr>
           <th>Task ID</th>
           <th>Task Name</th>
-          <th>Task Description Name</th>
+          <th>Description</th>
           <th>Completion Percentage</th>
+          <th>Weight Percentage</th>
         </tr>
       </thead>
       <tbody>
-        {tasks.map((task) => (
+        {tasks2.map((task) => (
           <tr key={task.id}>
             <td>{task.id}</td>
             <td>{task.name}</td>
             <td>{task.description}</td>
-            <td>{task.percent_completion}</td>
+            <td><ProgressBar animated now={task.percent_completion} label={task.percent_completion} variant={color(task.percent_completion,task.weight)}/></td>
+            <td><ProgressBar now={task.weight} label={`${task.weight}%`}/>;</td>
           </tr>
         ))}
-        
       </tbody>
     </Table>
         </Modal.Body>
